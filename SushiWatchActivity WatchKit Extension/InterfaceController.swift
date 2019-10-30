003915f9c9a8d411b076ac60f27863ed4a031bf7
ankitraj kaur <ankitrajkaur@gmail.com>
2019-10-30 13:00:10 -0400

    Initial Commit

---
+++ SushiWatchActivity WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SushiWatchActivity WatchKit Extension
+//
+//  Created by Sruthi on 2019-10-30.
+//  Copyright © 2019 Ankita. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
